d0a46070d4adc2fde78c1ec1437f6d8e02fa22ab
Marvin <info@marvinmetz.de>
2024-08-17 23:48:22 +0200

    feat(core): add ISqlDataContext.cs to provide support for all mssql server

---
+++ Core/UserDatabase/ISqlDataContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Core.UserDatabase;
+
+public interface ISqlDataContext : IDisposable
+{
+    IDataReader ExecuteReader(string cmdText, ICollection<SqlParameter> parameters);
+}
