56c28787ce445f9cc2d2db877b7d7bebc8b57cda
Marvin <info@marvinmetz.de>
2024-12-10 22:00:28 +0100

    feat(model): add model project with IDatabase.cs and ITable.cs
    refs: #14

---
+++ Model/UserDatabase/IDatabase.cs
@@ -0,0 +1,22 @@
+﻿namespace Model.UserDatabase;
+
+/// <summary>
+/// Represents model for a user database.
+/// </summary>
+public interface IDatabase
+{
+    /// <summary>
+    /// Gets the name of the database.
+    /// </summary>
+    public string? Name { get; }
+
+    /// <summary>
+    /// Gets the version of the database.
+    /// </summary>
+    public string? Version { get; }
+    
+    /// <summary>
+    /// Gets the tables of the database.
+    /// </summary>
+    public IEnumerable<ITable>? Tables { get; }
+}
